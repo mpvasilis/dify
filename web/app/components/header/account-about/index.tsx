2ae39be46a97090790f5435ac5b45c91198a3172
Vasilis Balafas <me@vasilis.pw>
2024-09-30 13:30:00 +0300

    refactor: remove unused footer code in page.tsx

--- web/app/components/header/account-about/index.tsx
+++ web/app/components/header/account-about/index.tsx
@@ -39,18 +39,7 @@ export default function AccountAbout({
           <LogoSite className='mx-auto mb-2' />
           <div className='mb-3 text-center text-xs font-normal text-gray-500'>Version {langeniusVersionInfo?.current_version}</div>
           <div className='mb-4 text-center text-xs font-normal text-gray-700'>
-            <div>© {dayjs().year()} LangGenius, Inc., Contributors.</div>
-            <div className='text-[#1C64F2]'>
-              {
-                IS_CE_EDITION
-                  ? <Link href={'https://github.com/langgenius/dify/blob/main/LICENSE'} target='_blank' rel='noopener noreferrer'>Open Source License</Link>
-                  : <>
-                    <Link href='https://dify.ai/privacy' target='_blank' rel='noopener noreferrer'>Privacy Policy</Link>,<span> </span>
-                    <Link href='https://dify.ai/terms' target='_blank' rel='noopener noreferrer'>Terms of Service</Link>
-                  </>
-              }
-            </div>
-          </div>
+
         </div>
         <div className='mb-4 -mx-8 h-[0.5px] bg-gray-200' />
         <div className='flex justify-between items-center'>
@@ -59,26 +48,6 @@ export default function AccountAbout({
               isLatest
                 ? t('common.about.latestAvailable', { version: langeniusVersionInfo.latest_version })
                 : t('common.about.nowAvailable', { version: langeniusVersionInfo.latest_version })
-            }
-          </div>
-          <div className='flex items-center'>
-            <Link
-              className={classNames(buttonClassName, 'mr-2')}
-              href={'https://github.com/langgenius/dify/releases'}
-              target='_blank' rel='noopener noreferrer'
-            >
-              {t('common.about.changeLog')}
-            </Link>
-            {
-              !isLatest && !IS_CE_EDITION && (
-                <Link
-                  className={classNames(buttonClassName, 'text-primary-600')}
-                  href={langeniusVersionInfo.release_notes}
-                  target='_blank' rel='noopener noreferrer'
-                >
-                  {t('common.about.updateNow')}
-                </Link>
-              )
             }
           </div>
         </div>
